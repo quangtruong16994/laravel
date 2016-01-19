552e9bf8142cf1fc5475d2e6f22eb6c4c1bbb946
quangtruong <quangtruong16994@gmail.com>
2016-01-19 10:04:12 +0700

    Cập nhật DB

--- ArticleDB.sql
+++ ArticleDB.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jan 17, 2016 at 07:01 PM
+-- Generation Time: Jan 19, 2016 at 06:02 AM
 -- Server version: 5.6.26
 -- PHP Version: 5.6.12
 
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `articles` (
   `creator_id` int(10) DEFAULT NULL,
   `created_date` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=46 ;
 
 --
 -- Dumping data for table `articles`
@@ -49,7 +49,8 @@ CREATE TABLE IF NOT EXISTS `articles` (
 
 INSERT INTO `articles` (`id`, `title`, `summary`, `content`, `alias`, `image`, `category_id`, `author`, `created_at`, `updated_at`, `status`, `creator_id`, `created_date`) VALUES
 (1, 'Phó Thủ tướng: Có quản lý thị trường "bảo kê" cho hàng giả!', 'Phó Thủ tướng Chính phủ Nguyễn Xuân Phúc đánh giá cao những thành tích đạt được của lực lượng Quản lý thị trường cả nước trong năm 2015. Tuy nhiên, một bộ phận của QLTT có tình trạng kiểm tra nhiều nhưng xử lý ít, còn gây phiền hà, bảo kê, tiếp tay cho các tiêu cực…', 'Trong năm 2015, lực lượng quản lý thị trường (QLTT) trên cả nước đã kiểm tra hơn 174.000 vụ (tăng hơn 5.400 vụ, tăng 3,2% so với năm 2014); phát hiện, xử lý hơn 103.700 vụ vi phạm (tăng gần 10.500 vụ, tăng 11,2% so với năm 2014); tổng số thu nộp ngân sách gần 460 tỷ đồng (tăng 63,5 tỷ, tăng 16% so với năm 2014); giá trị hàng tịch thu chưa bán 133,8 tỷ đồng (tăng 44,5 tỷ đồng, tăng 59,5% so với năm 2014).\r\nĐối với công tác buôn lậu thuốc lá, Phó Thủ tướng Chính phủ Nguyễn Xuân Phúc – Trưởng ban Chỉ đạo 389 (BCĐ 389) quốc gia (Ban Chỉ đạo quốc gia chống buôn lậu, gian lận thương mại và hàng giả) đã hoan nghênh và biểu dương các Bộ, ngành, lực lượng chức năng và các địa phương đã nghiêm túc triển khai có hiệu quả Chỉ thị số 30/CT-TTg của Thủ tướng Chính phủ. Kết quả công tác chống buôn lậu thuốc lá đã giảm 30% lượng thuốc lá nhập lậu, trong đó lực lượng QLTT đã góp phần bắt giữ trên 1,9 triệu bao.\r\n\r\nCông tác phổ biến tuyên truyền pháp luật thực hiện tốt. Tổ chức ký cam kết hàng trăm nghìn đối tượng kinh doanh tập trung vào cơ sở kinh doanh mặt hàng thuốc lá, phân bón, mũ bảo hiểm, các siêu thị, trung tâm thương mại... Hoạt động kiểm tra việc chấp hành các nội dung ký cam kết được lực lượng QLTT quan tâm kiểm tra.', '', '', 1, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, NULL, NULL),
-(2, 'Vô Song Kiếm - Game Việt ấn định ra mắt vào 12/01', 'Vô Song Kiếm đã sẵn sàng để ra mắt phiên bản chính thức vào 12/01 tới. Được biết, đây là game mobile mới nhất của Studio Việt GameVN và NPH Vega Game.', 'Su khi kết thúc giai đoạn Alpha Test, Vô Song Kiếm đã sẵn sàng để ra mắt phiên bản chính thức vào 12/01 tới. Được biết, đây là game mobile mới nhất của Studio Việt GameVN và NPH Vega Game.\r\nVới những fan hâm mộ bộ truyện tranh 7 Viên ngọc rồng, có lẽ Lưỡng Long Nhất Thể là tuyệt kỹ ấn tượng thứ hai chỉ đứng sau chưởng lực Kamejoko (Kamehameha) của khỉ con Songoku. Lấy ý tưởng từ tuyệt chiêu này, đội ngũ phát triển của GameVN đã xây dựng một phiên bản game Kim Dung với tính năng Song Kiếm Hợp Bích độc đáo: 2 hoặc 3 nhân vật có mối lương duyên chuẩn xác với nhau khi ra trận có thể cùng hợp thể và tung ra một tuyệt kỹ cực mạnh. Đòn combo đầy uy lực này có thể thổi bay toàn bộ đội hình địch, với điều kiện nhân vật phải được kích đầy thanh nộ khí. Được biết, đòn combo mạnh nhất game đang thuộc sở hữu của 3 huynh đệ Kiều Phong - Đoàn Dự - Hư Trúc mang tên Quần Long Chi Thủ.\r\nMột ưu điểm không thể bỏ qua của Vô Song Kiếm là mỗi nhân vật và chiêu thức của game đều được lồng tiếng rất nghiêm túc, đúng chất võ lâm kiếm hiệp. Sở dĩ để làm được điều này, Vô Song Kiếm đã phải thuê lại đội ngũ chuyên lồng tiếng phim kiếm hiệp truyền hình để có được chất giọng chuẩn và quen thuộc nhất cho người nghe. Các nhân vật trong game cũng rất đa dạng phong phú, từ Thần Điêu Đại Hiệp, Thiên Long Bát Bộ... đến những nhân vật của Lộc Đỉnh Ký, Hiệp Khách Hành đều được xuất hiện và thể hiện hết sức sắc nét.', '', '', 2, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, NULL, NULL);
+(2, 'Vô Song Kiếm - Game Việt ấn định ra mắt vào 12/01', 'Vô Song Kiếm đã sẵn sàng để ra mắt phiên bản chính thức vào 12/01 tới. Được biết, đây là game mobile mới nhất của Studio Việt GameVN và NPH Vega Game.', 'Su khi kết thúc giai đoạn Alpha Test, Vô Song Kiếm đã sẵn sàng để ra mắt phiên bản chính thức vào 12/01 tới. Được biết, đây là game mobile mới nhất của Studio Việt GameVN và NPH Vega Game.\r\nVới những fan hâm mộ bộ truyện tranh 7 Viên ngọc rồng, có lẽ Lưỡng Long Nhất Thể là tuyệt kỹ ấn tượng thứ hai chỉ đứng sau chưởng lực Kamejoko (Kamehameha) của khỉ con Songoku. Lấy ý tưởng từ tuyệt chiêu này, đội ngũ phát triển của GameVN đã xây dựng một phiên bản game Kim Dung với tính năng Song Kiếm Hợp Bích độc đáo: 2 hoặc 3 nhân vật có mối lương duyên chuẩn xác với nhau khi ra trận có thể cùng hợp thể và tung ra một tuyệt kỹ cực mạnh. Đòn combo đầy uy lực này có thể thổi bay toàn bộ đội hình địch, với điều kiện nhân vật phải được kích đầy thanh nộ khí. Được biết, đòn combo mạnh nhất game đang thuộc sở hữu của 3 huynh đệ Kiều Phong - Đoàn Dự - Hư Trúc mang tên Quần Long Chi Thủ.\r\nMột ưu điểm không thể bỏ qua của Vô Song Kiếm là mỗi nhân vật và chiêu thức của game đều được lồng tiếng rất nghiêm túc, đúng chất võ lâm kiếm hiệp. Sở dĩ để làm được điều này, Vô Song Kiếm đã phải thuê lại đội ngũ chuyên lồng tiếng phim kiếm hiệp truyền hình để có được chất giọng chuẩn và quen thuộc nhất cho người nghe. Các nhân vật trong game cũng rất đa dạng phong phú, từ Thần Điêu Đại Hiệp, Thiên Long Bát Bộ... đến những nhân vật của Lộc Đỉnh Ký, Hiệp Khách Hành đều được xuất hiện và thể hiện hết sức sắc nét.', '', '', 2, '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 0, NULL, NULL),
+(45, 'Những game online bắn súng có đồ họa đẹp đến ngỡ ngàng', 'Tất cả những tựa game online bắn súng này đều xứng đáng thử qua trong thời gian ngắn tới đây', '<p><strong>Firefall</strong></p>\r\n\r\n<p><strong><a href="http://gamek.vn/firefall.htm" target="_blank" title="Firefall">Firefall</a></strong>&nbsp;l&agrave; một&nbsp;<a href="http://gamek.vn/game-ban-sung.htm" target="_blank" title="game bắn súng">game bắn s&uacute;ng</a>&nbsp;kết hợp nhập vai c&oacute; cả g&oacute;c nh&igrave;n thứ 1 lẫn thứ 3. Game sở hữu đồ họa b&oacute;ng bẩy v&agrave; nhiều t&iacute;nh năng đột ph&aacute; như thế giới mở chứ kh&ocirc;ng g&oacute;i gọn theo h&igrave;nh thức room truyền thống.</p>\r\n\r\n<p><strong>Firefall</strong>&nbsp;lấy cốt truyện khoa học viễn tưởng trong tương lai, khi m&agrave; người Tr&aacute;i Đất bắt đầu kh&aacute;m ph&aacute; vũ trụ, họ ph&aacute;t hiện ra những h&agrave;nh tinh mới, nhiều tiềm năng nhưng cũng phải đối mặt với lũ qu&aacute;i vật đang ngự trị tr&ecirc;n h&agrave;nh tinh n&agrave;y.</p>\r\n', 'nhung-game-online-ban-sung-co-do-hoa-dep-den-ngo-ngang', '', 2, 'gamek', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 1, 1, '2016-01-18 21:40:30');
 
 -- --------------------------------------------------------
 
@@ -132,6 +133,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `birthday` datetime NOT NULL,
   `created_at` timestamp NOT NULL,
   `updated_at` timestamp NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
@@ -139,15 +141,15 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `email`, `fullname`, `phone`, `address`, `birthday`, `created_at`, `updated_at`) VALUES
-(1, 'admin', '123456', 'quangtruong@gmail.com', 'Đỗ Quang Trường', '01234567899', 'Hà Nội', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(2, 'e91D45eTPY', '$2y$10$A8651Uj8SyeMqlUu7SwikOV3Ew1.hxl5ysOK1XbWGHGcrnZ7MF/X6', '2GoHa1FSBu@gmail.com', 'hZ4xdHltEgUEQri', '01698202514', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(3, 'Hw6jSHH4jB', '$2y$10$GPQrkSRYfHtXc2mkmZVIp.cK4TgezbEt0Jyi6caal6.ioRGdzBRTm', 'N1aFxVAQRf@gmail.com', 'qjWb77bNt6xOK86', '01649560546', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(4, 'xATMl66B9g', '$2y$10$gZaD8tEyzbkol28UxhL4pOoqSbAHQ4kw7slO4ZLpsM4t7kk90DREi', 'qsP5NRuKOM@gmail.com', 'K0qc8TQ63coAopf', '01664773559', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(5, '3ITyF7W2uN', '$2y$10$PPi6ixcpwffmAwVOQGfri.GuZS1qtKpdoFIwVgm23.mlROJX3adaW', 'cVoszGS6Pu@gmail.com', 'at30NcjAXmTstKm', '01696429443', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(6, 'R9pxc2rITO', '$2y$10$6R2DifgmC1uLyKYtzBI0KuH2sR6Vn/4xS0oaYEDK24KwL5nLF0q2O', 'AdWNO8jr15@gmail.com', 'iDSw7GtUonjf3jp', '01651608276', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(7, 'Lkf2RAu5qB', '$2y$10$Byz6kMMlBHbvhI2aKoE2G.54P1.Di4q2hvEGlieKziK.szxhr48f2', 'VhDmvDYlPH@gmail.com', 'RxnOPN7I6eajUC8', '01677038574', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
-(8, 'JONmAQUags', '$2y$10$ztS5DohVns4FUxYVvBmUw.wD7yTQlNOkEyb6dvooCN91eS0elJmgu', 'WY7OKjS4Xe@gmail.com', 'WshJOnPogRpjIQY', '01641128540', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `fullname`, `phone`, `address`, `birthday`, `created_at`, `updated_at`, `remember_token`) VALUES
+(1, 'admin', '$2y$10$V4UoNzzVZUEQS5mJlDVCoOtbwBDxM/It.aiedoA5e/3mZC/mfN4ZS', 'quangtruong@gmail.com', 'Đỗ Quang Trường', '01234567899', 'Hà Nội', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '2016-01-19 02:21:46', 'ue6H0QJamXc5SUI74qBraaK638d9RoZnBKDZFXDZAn0Ey9QoQZBU2RQtHFXg'),
+(2, 'e91D45eTPY', '$2y$10$A8651Uj8SyeMqlUu7SwikOV3Ew1.hxl5ysOK1XbWGHGcrnZ7MF/X6', '2GoHa1FSBu@gmail.com', 'hZ4xdHltEgUEQri', '01698202514', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(3, 'Hw6jSHH4jB', '$2y$10$GPQrkSRYfHtXc2mkmZVIp.cK4TgezbEt0Jyi6caal6.ioRGdzBRTm', 'N1aFxVAQRf@gmail.com', 'qjWb77bNt6xOK86', '01649560546', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(4, 'xATMl66B9g', '$2y$10$gZaD8tEyzbkol28UxhL4pOoqSbAHQ4kw7slO4ZLpsM4t7kk90DREi', 'qsP5NRuKOM@gmail.com', 'K0qc8TQ63coAopf', '01664773559', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(5, '3ITyF7W2uN', '$2y$10$PPi6ixcpwffmAwVOQGfri.GuZS1qtKpdoFIwVgm23.mlROJX3adaW', 'cVoszGS6Pu@gmail.com', 'at30NcjAXmTstKm', '01696429443', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(6, 'R9pxc2rITO', '$2y$10$6R2DifgmC1uLyKYtzBI0KuH2sR6Vn/4xS0oaYEDK24KwL5nLF0q2O', 'AdWNO8jr15@gmail.com', 'iDSw7GtUonjf3jp', '01651608276', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(7, 'Lkf2RAu5qB', '$2y$10$Byz6kMMlBHbvhI2aKoE2G.54P1.Di4q2hvEGlieKziK.szxhr48f2', 'VhDmvDYlPH@gmail.com', 'RxnOPN7I6eajUC8', '01677038574', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL),
+(8, 'JONmAQUags', '$2y$10$ztS5DohVns4FUxYVvBmUw.wD7yTQlNOkEyb6dvooCN91eS0elJmgu', 'WY7OKjS4Xe@gmail.com', 'WshJOnPogRpjIQY', '01641128540', '', '0000-00-00 00:00:00', '0000-00-00 00:00:00', '0000-00-00 00:00:00', NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
